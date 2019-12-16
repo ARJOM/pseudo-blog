6fa435311fba9f92356104ce9f4a322fbf655ee2
Ricart <ricarteusuo11@gmail.com>
2019-12-16 11:23:36 -0300

    Atualizando estilo e organização do site

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
-import {MuiThemeProvider} from '@material-ui/core';
+import {IconButton, MuiThemeProvider} from '@material-ui/core';
 import {AppBar, Toolbar, Typography} from '@material-ui/core';
 import {createMuiTheme} from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 
 import FirebaseService from "../../services/FirebaseService";
+
+import Home from "../Home/Home";
+import {Link} from "react-router-dom";
+import {urls} from "../../util/urlUtils";
+import MenuIcon from "@material-ui/icons/Menu";
+import {Post} from "../Posts/Post";
+import Add from "../Add/Add";
+import Register from "../Register/Register";
+
+import {Card, CardContent} from "@material-ui/core";
 
 const theme = createMuiTheme({
   palette: {
@@ -28,13 +39,50 @@ export default class App extends Component {
     return (
         <MuiThemeProvider theme={theme}>
           <React.Fragment>
-            <AppBar position="static">
+            <AppBar position="static" id="titulo">
               <Toolbar>
+
+                <IconButton color="inherit" aria-label="Menu"
+                            component={props =>
+                                <Link to={urls.home.path} {...props}/>}
+                >
+                  <MenuIcon/>
+                </IconButton>
+
                 <Typography type="title" color="inherit">
-                  My Awesome React App
+                  Pseudo-Blog
                 </Typography>
+
               </Toolbar>
             </AppBar>
+            <Card className="card">
+              <CardContent>
+
+                <Route exact
+                       path={urls.home.path}
+                       render={(props) =>
+                           <Home {...props}/>}
+                />
+
+                <Route exact
+                       path={urls.post.path}
+                       render={(props) =>
+                           <Post {...props} data={this.state.data}/>}
+                />
+
+                <Route exact
+                       path={urls.add.path}
+                       render={(props) =>
+                           <Add {...props}/>}
+                />
+                <Route exact
+                       path={urls.register.path}
+                       render={(props) =>
+                       <Register {...props}/>}
+                />
+              </CardContent>
+            </Card>
+
           </React.Fragment>
         </MuiThemeProvider>
     );
