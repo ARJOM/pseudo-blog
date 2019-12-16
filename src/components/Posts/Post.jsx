a76474fc94f950922722856cd2cb57217635ec33
Ricart <ricarteusuo11@gmail.com>
2019-12-16 11:45:49 -0300

    Criando componente base para publicação

---
+++ src/components/Posts/Post.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./Post.css"
+
+export const Post = ({data}) => {
+    return(
+        <Fragment>
+            <div className="post">
+                <h2>Post</h2>
+                <p>Conteúdo</p>
+            </div>
+        </Fragment>
+    )
+};
